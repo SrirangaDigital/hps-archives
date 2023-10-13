5be13e8e46b5a4442981cd0291cf1aa267bfdadb
sriranga <sriranga@Latitude-E7470>
2023-10-13 14:14:52 +0530

    pages updated

--- application/views/flat/Documents/index.php
+++ application/views/flat/Documents/index.php
@@ -9,25 +9,8 @@
 <div class="container gap-above">
     <div class="row">
         <div class="col-md-12 main">
-            <h1>Documents</h1>
-		    <div class="row">
-                <div class="col-md-4 main">
-                    <div class="thumbnail">
-                        <img src="<?=PUBLIC_URL?>images/stock/one.jpg" alt="Lights" style="width:100%">
-                        <div class="caption">
-                          <strong><center><p style="font-size: 22px;">The Eaglet</p></center></strong>
-                      </div>
-                  </div>
-              </div>
-                <div class="col-md-4 main">
-                    <div class="thumbnail">
-                        <img src="<?=PUBLIC_URL?>images/stock/two.jpg" alt="Lights" style="width:100%">
-                        <div class="caption">
-                          <strong><center><p style="font-size: 22px;">The Eagle</p></center></strong>
-                      </div>
-                  </div>
-              </div>
-            </div>
+            <h1>Documents - Under Process</h1>
+		    
         </div>
     </div>
 </div>
