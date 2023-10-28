abcf18f26fa6b7ec6ad7aff29052dec99b9db13d
sriranga <sriranga@Latitude-E7470>
2023-10-28 15:22:30 +0530

    mongo db changed

--- application/config/config.php
+++ application/config/config.php
@@ -19,16 +19,13 @@ define('PHY_JSON_PRECAST_URL', PHY_BASE_URL . 'json-precast/');
 define('PHY_FLAT_URL', PHY_BASE_URL . 'application/views/flat/');
 define('PHY_STOCK_IMAGE_URL', PHY_PUBLIC_URL . 'images/stock/');
 
+
+
 define('DB_HOST', 'localhost');
 define('DB_PORT', '27017');
-define('DB_NAME', 'iitmARCHIVES');
-define('DB_USER', 'iitmUSER');
-define('DB_PASSWORD', 'iitm123');
+define('DB_NAME', 'hpsARCHIVES');
+define('DB_USER', 'hpsUSER');
+define('DB_PASSWORD', 'hps123');
 
-// Git config
-define('GIT_USER_NAME', 'sriranga-deploy');
-define('GIT_PASSWORD', 'xxx');
-define('GIT_REPO', 'github.com/SrirangaDigital/iitm-nosql.git');
-define('GIT_REMOTE', 'https://' . GIT_USER_NAME . ':' . GIT_PASSWORD . '@' . GIT_REPO);
-define('GIT_EMAIL', 'accounts@srirangadigital.com');
+
 ?>
