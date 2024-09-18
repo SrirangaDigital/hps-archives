c3a878f8dbb330e2b4f1dd04430f3d808dc167f9
sriranga <sriranga@Latitude-E7470>
2024-09-18 17:10:37 +0530

    Home page view updated

--- application/views/flat/1-Home/index.php
+++ application/views/flat/1-Home/index.php
@@ -1,26 +1,42 @@
 <script src='https://www.google.com/recaptcha/api.js'></script>
 <link href="https://use.fontawesome.com/releases/v5.0.4/css/all.css" rel="stylesheet">
 
-<!-- <div class="container-fluid" id="home">
-    <div class="row">
-        <div class="col-md-12 clear-paddings">
-            <div class="fixOverlayDiv">
-                <img class="img-responsive gap-above" src="<?=PUBLIC_URL?>images/stock/slide1.jpg">
-                <div class="OverlayTextMain">
-                    <div class="mainpage">
-                        <h2>Hyderabad Public School</h2>
-                        <p>The Journey of a Century , Established in 1923</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div> -->
-
-<div id="carouselExample" class="carousel slide ">
+<!-- <style type="text/css">
+    .carousel > .carousel-inner > .carousel-item > img{
+    width:240px; /* Yeap you can change the width and height*/
+    height:160px;
+}
+</style> -->
+<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
     <div class="carousel-inner">
         <div class="carousel-item active">
-            <img src="<?=PUBLIC_URL?>images/stock/slide1.jpg" class="d-block w-100" alt="Building_image">
+            <img id="imght" src="<?=PUBLIC_URL?>images/stock/slidenew.jpg" class="d-block w-100 p-0" alt="backgroundimage">
+
+            <div class="OverlayTextMain" >
+                <h1>Hyderabad Public School</h1>
+                <p>
+                    The Journey of a Century , Established in 1923
+                </p>
+</div>
+</div>
+</div>
+</div>
+
+<div class="container-fluid blue text-center">
+    <h1 class='mt-1'>About</h1>
+    <p>The Hyderabad Public School is a co-educational, day & residential school in Hyderabad. One of the oldest educational institutions in the youngest state of the country – it is this range that best defines The Hyderabad Public School</p>
+    <p>In 1919, a proposal was presented to the Seventh Nizam, H.E.H. Mir Osman Ali Khan by the Hon’ble H. Weikfield, Director General of the Court of Wards (Department of Revenue) to establish a fully residential school on the lines of Eton College, London, to give the sons of Jagirdars an enriching learning experience. &nbsp;&nbsp;&nbsp;<a style="color: white; "href="<?=BASE_URL?>About">Read more...</a></p>
+</div>
+<hr class="border border-primary border-3 opacity-75">
+
+
+
+
+
+<!-- <div id="carouselExample" class="carousel slide ">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="<?=PUBLIC_URL?>images/stock/slidenew.jpg" class="d-block w-100" alt="Building_image">
             <div class="OverlayTextMain">
                 <div class="mainpage">
                     <h2>Hyderabad Public School</h2>
@@ -29,160 +45,116 @@
             </div>
         </div>
     </div>
-</div>
-
-<!-- <div class="container-fluid stats" id="collection">
-    <div class="row">
-        <div class="col-md-12">
-            <h1>Gallery</h1>
-            <ul class="list-inline">
-                <li class="stat-elem">
-                    <a href="<?=BASE_URL?>listing/categories/Photograph?select=Event">
-                        <h2><i class="fa fa-image"></i></h2>
-                        <p>Photographs</p>
-                    </a>
-                </li>
-                <li class="stat-elem">
-                    <a href="<?=BASE_URL?>listing/Categories/Magazine?select=Name">
-                        <h2><i class="fa fa-book"></i></h2>
-                        <p>Magazine</p>
-                    </a>
-                </li>
-                <li class="stat-elem">
-                    <a href="<?=BASE_URL?>listing/categories/Newspaper?select=Year">
-                        <h2><i class="fa fa-envelope"></i></h2>
-                        <p>News Paper Clippings</p>
-                    </a>
-                </li>
-            </ul>
-        </div>
-    </div>
 </div> -->
 
-
-
-
-
-
-
-        <!-- <div class="container-fluid stats" id="collection">
-            <div class="row clear-margins">
-                <div class="col-md-12 text-center">
-                    <h1>Gallery</h1>
-                    <div class="col-md-2"></div>
-                    <ul class="list-inline">
-                        <li class="stat-elem">
-                            <a href="<?=BASE_URL?>listing/categories/Photograph?select=Event">
-                                <h2><i class="fa fa-image"></i></h2>
-                                <p>Photographs</p>
-                            </a>
-                        </li>
-                        <li class="stat-elem">
-                           <a href="<?=BASE_URL?>listing/Categories/Magazine?select=Name">
-                               <h2><i class="fa fa-book"></i></h2>
-                               <p>Magazine</p>
-                           </a>
-                       </li>
-                       <li class="stat-elem">
-                        <a href="<?=BASE_URL?>listing/categories/Newspaper?select=Year">
-                                <h2><i class="fa fa-envelope"></i></h2>
-                                <p>News Paper Clippings</p>
-                            </a>
-                        </li>
-                    </ul>
-                    <div class="col-md-2"></div>
-                </div>text-end
-            </div>
+<!-- <div class="container-fluid stats text-center">
+    <div class="row"><br />
+        <h1 class="mt-5">Gallery</h1>
+        <div class="col-20 col"></div>
+        <div class="col-20 col stat-elem mt-5 ">
+            <a href="<?=BASE_URL?>listing/categories/Photograph?select=Event">
+                <h2><i class="fa fa-image"></i></h2>
+                <p class="">Photographs</p>
+            </a>
         </div>
- -->
-        <div class="container-fluid stats text-center">
-            <div class="row"><br />
-                <h1 class="mt-5">Gallery</h1>
-                <div class="col-20 col"></div>
-                <div class="col-20 col stat-elem mt-5 ">
-                    <a href="<?=BASE_URL?>listing/categories/Photograph?select=Event">
-                        <h2><i class="fa fa-image"></i></h2>
-                        <p class="">Photographs</p>
-                    </a>
-                </div>
-                <div class="col-20 col stat-elem mt-5">
-                    <a href="<?=BASE_URL?>listing/Categories/Magazine?select=Name">
-                     <h2><i class="fa fa-book"></i></h2>
-                     <p>Magazine</p>
-                 </a>
-                </div>
-                <div class="col-20 col stat-elem mt-5 ">
-                    <a href="<?=BASE_URL?>listing/categories/Newspaper?select=Year">
-                        <h2><i class="fa fa-envelope"></i></h2>
-                        <p class="">News Paper Clippings</p>
-                    </a>
-                </div>
-
-                <div class="col-20 col stat-elem mt-5 ">
-                    <a href="<?=BASE_URL?>listing/categories/CentenaryPhotograph?select=Caption">
-                        <h2><i class="fa fa-image"></i></h2>
-                        <p class="">Centenary Photographs</p>
-                    </a>
-                </div>
-
-
-                <div class="col-20 col" ></div>
-                
-            </div><br />
-            <div class="mt-5">&nbsp;</div>
+        <div class="col-20 col stat-elem mt-5">
+            <a href="<?=BASE_URL?>listing/Categories/Magazine?select=Name">
+                <h2><i class="fa fa-book"></i></h2>
+                <p>Magazine</p>
+            </a>
         </div>
 
-
-
-
-
-
-
-
-<!-- 	                        <li class="stat-elem">
-	                            <a href="<?=BASE_URL?>listing/artefacts/Multimedia%20File?Category=Oral+History+Archives">
-	                                <h2><i class="fa fa-volume-up"></i></h2>
-	                                <p>Oral History Archives</p>
-	                            </a>
-	                        </li>
-	                        <li class="stat-elem">
-	                            <a href="<?=BASE_URL?>listing/artefacts/Miscellaneous%20Video?Category=Videos">
-	                                <h2><i class="fa fa-video-camera"></i></h2>
-	                                <p>Videos</p>
-	                            </a>
-	                        </li>                        
-	                        <li class="stat-elem">
-	                            <a href="<?=BASE_URL?>Photoexhibition">
-	                                <h2><img class="exhibition-icon" src="<?=BASE_URL?>public/images/stock/exhibition-icon.png" alt="" /></h2>
-	                                <p>Photo Exhibition</p>
-	                            </a>
-	                        </li> -->
-                    </ul>
-                </div>
-                <!-- <div class="col-md-12">
-                    <ul class="list-inline">
-                        <li class="stat-elem">
-                            <a href="<?=BASE_URL?>DAA" target="_blank">
-                                <h2><i class="fa fa-trophy"></i></h2>
-                                <p>Distinguished Alumnus Awardees</p>
-                            </a>
-                        </li>
-                        <li class="stat-elem">
-                            <a href="<?=BASE_URL?>Letters_from_the_Heritage_Centre">
-                                <h2><i class="fa fa-envelope"></i></h2>
-                                <p>Letters from the Heritage Centre</p>
-                            </a>
-                        </li>
-                        <li class="stat-elem">
-                            <a href="<?=BASE_URL?>Downloads">
-                                <h2><i class="fa fa-download"></i></h2>
-                                <p>Downloads</p>
-                            </a>
-                        </li>
-                    </ul>
-                </div> -->
-            </div>
+        <div class="col-20 col stat-elem mt-5 ">
+            <a href="<?=BASE_URL?>listing/categories/CentenaryPhotograph?select=Event">
+                <h2><i class="fa fa-image"></i></h2>
+                <p class="">Centenary Photographs</p>
+            </a>
         </div>
+        <div class="col-20 col" ></div>
+    </div><br />
+    <div class="mt-5">&nbsp;</div>
+</div> -->
+
+<?php
+        if(!isset($_SESSION['login']))
+        {
+            echo"
+
+<div class='container-fluid stats p-2 text-center'>
+<h1 id='collection' class='mt-5'>Gallery</h1>
+  <div class='row'>
+    <div class='col stat-elem'>
+      <a href='listing/categories/Photograph?select=Event'>
+                <h2><i class='fa fa-image'></i></h2>
+                <p class=''>Photographs</p>
+            </a>
+    </div>
+    <div class='col stat-elem'>
+       <a href='listing/Categories/Magazine?select=Name'>
+                <h2><i class='fa fa-book'></i></h2>
+                <p>Magazine</p>
+            </a>
+    </div>
+    <div class='col stat-elem'>
+      <a href='listing/categories/CentenaryPhotograph?select=Event'>
+                <h2><i class='fa fa-image'></i></h2>
+                <p class=''>Centenary Photographs</p>
+            </a>
+    </div>
+  </div>
+</div>
+ ";
+        }
+        ?>
+
+
+
+
+
+
+<?php
+        if(isset($_SESSION['login']))
+        {
+            echo"
+
+<div class='container-fluid stats p-2 text-center'>
+<h1 id='collection' class='mt-5'>Gallery</h1>
+  <div class='row'>
+    <div class='col stat-elem'>
+      <a href='listing/categories/Photograph?select=Event'>
+                <h2><i class='fa fa-image'></i></h2>
+                <p class=''>Photographs</p>
+            </a>
+    </div>
+    <div class='col stat-elem'>
+       <a href='listing/Categories/Magazine?select=Name'>
+                <h2><i class='fa fa-book'></i></h2>
+                <p>Magazine</p>
+            </a>
+    </div>
+    <div class='col stat-elem'>
+      <a href='listing/categories/CentenaryPhotograph?select=Event'>
+                <h2><i class='fa fa-image'></i></h2>
+                <p class=''>Centenary Photographs</p>
+            </a>
+    </div>
+    <div class='col stat-elem'>
+       
+           
+            <a href='listing/categories/Newspaper?select=Year'>
+            <h2><i class='fa fa-envelope'></i></h2>
+            <p class=''>News Paper Clippings</p>
+            </a>
+           
+    </div>
+  </div>
+</div>
+ ";
+        }
+        ?>
+
+
+
 
 
 
